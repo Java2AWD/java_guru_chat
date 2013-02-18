9e83093db0904c25838ddb4b265a4df3f2be9580
Aleksey <xaotuk1@gmail.com>
2013-02-18 19:03:58 +0200

    User DAO classes

--- JavaGuruChat/src/com/seventysevenagency/chat/dao/UserDAO.java
+++ JavaGuruChat/src/com/seventysevenagency/chat/dao/UserDAO.java
@@ -1,5 +1,15 @@
 package com.seventysevenagency.chat.dao;
 
-public interface UserDAO {
+import com.seventysevenagency.chat.domain.User;
 
+public interface UserDAO {
+	public void create(User user);
+
+	public void update(User user);
+
+	public void find(int id);
+
+	public void delete(User user);
+
+	public void deleteById(int id);	
 }
---
+++ JavaGuruChat/src/com/seventysevenagency/chat/dao/UserDAOImpl.java
@@ -0,0 +1,37 @@
+package com.seventysevenagency.chat.dao;
+
+import com.seventysevenagency.chat.domain.User;
+
+public class UserDAOImpl implements UserDAO {
+
+	@Override
+	public void create(User user) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(User user) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void find(int id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(User user) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteById(int id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
