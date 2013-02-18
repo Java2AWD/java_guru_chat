ec2d35ac37de4ecdccce3b815904b80c718985f4
Aleksey <xaotuk1@gmail.com>
2013-02-18 20:25:21 +0200

    BaseDao class that manages connections

---
+++ JavaGuruChat/src/com/seventysevenagency/chat/dao/BaseDAO.java
@@ -0,0 +1,35 @@
+package com.seventysevenagency.chat.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BaseDAO {
+
+	public Connection getConnection() {
+		try {
+			Class.forName("com.postgresql.Driver");
+		} catch (ClassNotFoundException e1) {
+			throw new RuntimeException("Cannot find driver class", e1);
+		} 
+		String url = "jdbc:postgresql://localhost:5432/books";
+		String username = "devel";
+		String password = "banzaj";
+		Connection result;
+		try {
+			result = DriverManager
+					.getConnection(url, username, password);
+		} catch (SQLException e) {
+			throw new RuntimeException("Cannot get connection", e);
+		}
+		return result;
+	}
+	
+	public void closeConnection(Connection connection) {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			throw new RuntimeException("Cannot close connection", e);
+		}
+	}
+}
