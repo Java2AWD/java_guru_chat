1618f9969b9378e8dd09cddd81ce7c2c552df4d9
wooshie <wooshie@gmail.com>
2013-02-20 22:03:17 +0200

    Modified interface MessageDAO. Adding implementation of MessageDAO interface.

--- JavaGuruChat/src/com/seventysevenagency/chat/dao/MessageDAO.java
+++ JavaGuruChat/src/com/seventysevenagency/chat/dao/MessageDAO.java
@@ -7,7 +7,7 @@ import com.seventysevenagency.chat.domain.Message;
 import com.seventysevenagency.chat.domain.User;
 
 public interface MessageDAO {
-	public void create(Message message) throws DAOException;
+	public Long create(Message message) throws DAOException;
 
 	public void update(Message message) throws DAOException;
 
